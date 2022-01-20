1fe916f2d909b85f9f24f8e4b49892bbe6a38222
osm0nov1 <osmonovrashit@gmail.com>
2022-01-20 16:02:48 +0600

    Initial Commit

---
+++ homeWork5 mouth3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork5 mouth3
+//
+//  Created by Rashit Osmonov on 20/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
